18ad7689609221ccd2aae5883033d8b841427f31
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-02-26 05:52:39 -0600

    Added Alamofire connection

---
+++ healapp/Globals.swift
@@ -0,0 +1,11 @@
+//
+//  Globals.swift
+//  healapp
+//
+//  Created by Aqueel Miqdad on 2/26/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
+
+let DB_URL = "https://hackillinois-imo.herokuapp.com/empdb/employee"
